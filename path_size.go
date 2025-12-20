8d3fb2ada2bb2cfc93833e061ea5790c2f54abd4
Maksim Kotov <4253046+aksioto@users.noreply.github.com>
2025-12-20 03:02:00 +0300

    Step 3

--- path_size.go
+++ path_size.go
@@ -1,1 +1,40 @@
 package code
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+)
+
+func GetSize(path string) int64 {
+	var size int64
+
+	fi, err := os.Lstat(path)
+	if err != nil {
+		return size
+	}
+
+	switch mode := fi.Mode(); {
+	case mode.IsRegular():
+		size = fi.Size()
+
+	case mode&fs.ModeSymlink != 0:
+		fmt.Println("symbolic link")
+
+	case mode.IsDir():
+		files, err := os.ReadDir(path)
+		if err != nil {
+			return size
+		}
+
+		for _, file := range files {
+			info, err := file.Info()
+			if err != nil {
+				continue
+			}
+			size += info.Size()
+		}
+	}
+
+	return size
+}
